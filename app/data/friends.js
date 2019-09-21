3a2d65366bae8e7cd8815ea0063f2918b76ace62
Colin Cason <colinjcason@gmail.com>
2019-09-21 11:52:09 -0700

    built html

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+    {
+        "name": "joker", 
+        "photo": "../images/joker.jpg",
+        "score": [5, 4, 2, 4, 3, 1, 2, 3, 5, 4]
+    },
+    {
+        "name": "captain-america", 
+        "photo": "../images/captain-america.jpg",
+        "score": [2, 3, 4, 5, 1, 3, 2, 1, 5, 4]
+    },
+    {
+        "name": "rocket", 
+        "photo": "../images/rocket.jpg",
+        "score": [5, 5, 2, 3, 4, 1, 5, 4, 2, 3]
+    }
+];
+
+module.exports = friends;
