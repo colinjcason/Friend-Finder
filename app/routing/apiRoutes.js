b5e6691032f40a53f1d07b1d3cb6691544b96b91
Colin Cason <colinjcason@gmail.com>
2019-09-23 20:13:03 -0700

    fixed another modal issue

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,33 +1,33 @@
 var friends = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
-    app.post("/api/friends", function(req, res) {
+    app.post("/api/friends", function (req, res) {
         var userScore = req.body.scores;
         var scoreArr = [];
         var bestMatch = 0;
 
-        for(var i = 0; i < friends.length; i++) {
+        for (var i = 0; i < friends.length; i++) {
             var diff = 0;
 
-            for(var j = 0; j < userScore.length; j++) {
+            for (var j = 0; j < userScore.length; j++) {
                 diff += Math.abs(friends[i].score[j] - userScore[j]);
             }
             scoreArr.push(diff);
         }
 
-        for(var i = 0; i < scoreArr.length; i++) {
-            if(scoreArr[i] <= scoreArr[bestMatch]) {
+        for (var i = 0; i < scoreArr.length; i++) {
+            if (scoreArr[i] <= scoreArr[bestMatch]) {
                 bestMatch = i;
             }
         }
 
         var bestFriend = friends[bestMatch];
         res.json(bestFriend);
-    
+
     });
 }
